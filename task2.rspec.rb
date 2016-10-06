23696b95529e0b58ad17a3bf0fb0c79aefeb1914
BjornMelgaard <melgaardbjorn@gmail.com>
2016-10-06 07:53:28 +0300

    task2 init

---
+++ task2.rspec.rb
@@ -0,0 +1,14 @@
+require_relative 'task2'
+
+describe Factory do
+  context 'created structure' do
+    it 'have given class variables' do
+    end
+
+    it "can evaluate passed block in it's context" do
+    end
+
+    it 'afford access by index' do
+    end
+  end
+end
