87e225fc6a22d34ca72a57d8b60d65dfddbaccce
BjornMelgaard <melgaardbjorn@gmail.com>
2016-10-07 13:46:21 +0300

    stage: minimum viable product

--- task2.rb
+++ task2.rb
@@ -1,2 +1,22 @@
+#
 class Factory
+  def self.new(*f_args, &block)
+    klass = Class.new do
+      define_method :initialize do |*i_args|
+        f_args.each_with_index { |name, i| instance_variable_set("@#{name}", i_args[i]) }
+      end
+
+      f_args.each do |arg|
+        class_eval("def #{arg};@#{arg};end") # getter
+        class_eval("def #{arg}=(val);@#{arg}=val;end") # setter
+      end
+
+      def [](param)
+        param = instance_variables[param][1..-1] if param.is_a? Numeric
+        instance_variable_get("@#{param}")
+      end
+    end
+    klass.class_eval(&block) if block_given?
+    klass
+  end
 end
--- task2.rspec.rb
+++ task2.rspec.rb
@@ -1,14 +1,22 @@
 require_relative 'task2'
 
 describe Factory do
-  context 'created structure' do
-    it 'have given class variables' do
+  it 'have given class variables' do
+    Customer = Factory.new(:name, :address, :zip) do
+      def greeting
+        "Hello #{name}!"
+      end
     end
+    puts "Customer ancestors = #{Customer.ancestors.join(', ')}"
 
-    it "can evaluate passed block in it's context" do
+    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
+    [:name, :address, :zip].each do |arg|
+      expect(joe.instance_variable_defined?("@#{arg}")).to be_truthy
     end
-
-    it 'afford access by index' do
-    end
+    expect(joe.name).to     eq('Joe Smith')
+    expect(joe['name']).to  eq('Joe Smith')
+    expect(joe[:name]).to   eq('Joe Smith')
+    expect(joe[0]).to       eq('Joe Smith')
+    expect(joe.greeting).to eq('Hello Joe Smith!')
   end
 end
