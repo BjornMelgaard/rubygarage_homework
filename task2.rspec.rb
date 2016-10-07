6d8c4f28f7c73a53b07185aa80945075f21e2ee3
BjornMelgaard <melgaardbjorn@gmail.com>
2016-10-07 21:38:34 +0300

    tests refactoring

--- task2.rspec.rb
+++ task2.rspec.rb
@@ -1,22 +1,41 @@
 require_relative 'task2'
 
 describe Factory do
-  it 'have given class variables' do
-    Customer = Factory.new(:name, :address, :zip) do
-      def greeting
-        "Hello #{name}!"
+  after(:each) do
+    Object.send(:remove_const, :Customer)
+  end
+
+  it 'can create class without instance variables' do
+    Customer = Factory.new
+    customer = Customer.new
+    expect(customer.instance_variables).to eq([])
+  end
+
+  context 'created class instance' do
+    before(:each) do
+      Customer = Factory.new(:name, :address, :zip) do
+        def greeting
+          "Hello #{name}!"
+        end
       end
     end
-    puts "Customer ancestors = #{Customer.ancestors.join(', ')}"
+    let(:joe) { Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345) }
 
-    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
-    [:name, :address, :zip].each do |arg|
-      expect(joe.instance_variable_defined?("@#{arg}")).to be_truthy
+    it 'have instance variables' do
+      [:name, :address, :zip].each do |arg|
+        expect(joe.instance_variable_defined?("@#{arg}")).to be_truthy
+      end
+      expect(joe.name).to     eq('Joe Smith')
     end
-    expect(joe.name).to     eq('Joe Smith')
-    expect(joe['name']).to  eq('Joe Smith')
-    expect(joe[:name]).to   eq('Joe Smith')
-    expect(joe[0]).to       eq('Joe Smith')
-    expect(joe.greeting).to eq('Hello Joe Smith!')
+
+    it 'respond with []' do
+      expect(joe['name']).to  eq('Joe Smith')
+      expect(joe[:name]).to   eq('Joe Smith')
+      expect(joe[0]).to       eq('Joe Smith')
+    end
+
+    it 'have methods from block' do
+      expect(joe.greeting).to eq('Hello Joe Smith!')
+    end
   end
 end
